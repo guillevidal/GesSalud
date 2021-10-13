c375d746dce916b89a89cbab5ddec06d8991e0dd
roezna <rodrigoezequielnavarro99@gmail.com>
2021-10-13 20:05:08 -0300

    Instalando dependencias, creacion de carpeta components y archivos scss

---
+++ client/src/components/Landing/Landing.jsx
@@ -0,0 +1,23 @@
+import './Landing'
+export default function Landing(){
+
+    return(
+        <div id='landing-container'>
+
+            <div id='landing-header'>
+                
+            </div>
+            
+            <div>
+                <div id='landing-background'>
+
+                </div>
+                <div id='landing-login'>
+
+                </div>
+            </div>
+
+        </div>
+    )
+
+} 
