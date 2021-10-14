7aefb6fef656b6f46cfcb2b75fec6d82cf3223ed
roezna <rodrigoezequielnavarro99@gmail.com>
2021-10-13 23:29:00 -0300

    Creacion de estructura de Landing con respectivo Login

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -1,21 +1,59 @@
 import './Landing'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faClinicMedical,faUserMd} from '@fortawesome/free-solid-svg-icons'
+import { useState } from 'react'
+
 export default function Landing(){
+
+    const [input, setInput] = useState({
+        user : null,
+        pass : null
+    })
+
+    const handleChange = (e) => {
+        e.preventDefault()
+
+        setInput({
+            ...input,
+            [e.target.name] : e.target.value
+        })
+        
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        alert('ACCION EN ESPERA DE RESOLUCION')
+    }
 
     return(
         <div id='landing-container'>
-
             <div id='landing-header'>
-                
+                <div id='landing-title'>
+                    <FontAwesomeIcon icon={faClinicMedical} className='icon-salud' />
+                    <span className='landing-title-text'>GesSalud</span>
+                </div>
             </div>
             
-            <div>
-                <div id='landing-background'>
-
-                </div>
                 <div id='landing-login'>
 
+                        <form className='landing-form'>
+                             <FontAwesomeIcon icon={faUserMd} className='icon-login' />
+                            <label htmlFor="" className='form-title'>Iniciar Sesión</label>
+                            <div className='landing-form-inputs'>
+                                <div className='label-input'>
+                                    <label htmlFor="" className='label'>Usuario</label>
+                                    <input type="text" name='user' value={input.user} className='input' onChange={e => handleChange(e)}/>
+                                </div>
+                                <div className='label-input'>
+                                    <label htmlFor="" className='label'>Contraseña</label>
+                                    <input type="password" name='pass' value={input.pass} className='input' onChange={e => handleChange(e)}/>                   
+                                </div>
+                            </div>
+                            <button className='boton-login' onClick={e => handleSubmit(e)}>Ingresar</button>
+                        </form>
+
                 </div>
-            </div>
 
         </div>
     )
