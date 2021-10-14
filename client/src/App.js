6ad3a277a3325540316ba71cd89d71c7a98b9790
Nataramirez <nramirezlievano@gmail.com>
2021-10-13 22:57:26 -0500

    creacion de ruta formulario y creación de componentes formulario

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
 import { Route } from 'react-router-dom';
 import Landing from './components/Landing/Landing';
+import CreateSpecialist from './components/Forms/CreateSpecialist/CreateSpecialist'
 
 function App() {
   return (
     <div className="App">
 
-    <Route exact path='/'>
-      <Landing />
-    </Route>
+      <Route exact path='/'>
+        <Landing />
+      </Route>
+      <Route exact path='/createSpecialist'>
+     
+        <CreateSpecialist />
+      </Route>
 
     </div>
   );
