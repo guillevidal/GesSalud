2ca74ff222c4374e98e38f8a78529b7874915b2f
roezna <rodrigoezequielnavarro99@gmail.com>
2021-10-22 03:05:40 -0300

    Definiendo visual de especialista, modificacion de datos a la espera de carga de imagen

--- client/src/components/specialist/HomeSpecialist.jsx
+++ client/src/components/specialist/HomeSpecialist.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react"
 import { useDispatch } from "react-redux"
-import { especialistaDetallado, obtenerEspecialistas } from "../../actions"
+import { especialistaDetallado } from "../../actions"
 import Nav from "../Layout/Nav"
 import './HomeSpecialist.scss'
 
@@ -8,9 +8,8 @@ export default function HomeSpecialist(){
 
     const dispatch = useDispatch()
 
-    useEffect(async()=>{
-        await dispatch(obtenerEspecialistas())
-        await dispatch(especialistaDetallado(1))
+    useEffect(()=>{
+         dispatch(especialistaDetallado(1))
     },[])
 
     return(
