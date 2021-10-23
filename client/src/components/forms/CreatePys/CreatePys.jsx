ac388377c9db44bc82a7974d10c7d21328e4bb1a
garzon <jeangarzon24@gmail.com>
2021-10-22 19:28:38 -0500

    Se quitaron algunas advertencias en consola

--- client/src/components/forms/CreatePys/CreatePys.jsx
+++ client/src/components/forms/CreatePys/CreatePys.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Person from "../Person/Person.jsx";
--- client/src/components/forms/CreateSpecialist/CreateSpecialist.jsx
+++ client/src/components/forms/CreateSpecialist/CreateSpecialist.jsx
@@ -5,8 +5,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import Person from '../Person/Person';
 import {
     crearEspecialista,
-    obtenerEspecialidades, obtenerEspecialistas,
-} from '../../../actions/index'
+} from '../../../actions/index.js'
 
 import swal from 'sweetalert';
 
@@ -36,10 +35,6 @@ export default function CreateSpecialist() {
     const typeSpecialties = useSelector((state) => state.especialidades)
     const especialistas = useSelector(state => state.especialistas)
 
-    useEffect(() => {
-        dispatch(obtenerEspecialidades())
-
-    }, [])
 
     useEffect(() => {
         if (input.specialty.error) {
