4e388a694754c7a1549d4dc4975386f82407cca6
johaolopz <johao.lopz.m@gmail.com>
2021-10-25 13:12:47 -0500

    Actualizacion de valores roles y json de autenticar con id del especialista

--- api/src/routes/autenticar.js
+++ api/src/routes/autenticar.js
@@ -1,32 +1,51 @@
 const { Router } = require("express");
 const router = Router();
-const jwt = require('jsonwebtoken');
-const config = require('../configs/config');
-const { Persona } = require("../db");
+const jwt = require("jsonwebtoken");
+const config = require("../configs/config");
+const { Persona, Especialista_medico } = require("../db");
 
 router.post("/", async (req, res) => {
-    const register = await Persona.findOne({
-        where : { user : req.body.usuario}});
+  let register = undefined;
+  const persona = await Persona.findOne({
+    where: { user: req.body.usuario },
+  });
 
-    if (register) {
-        if (register.dataValues.password === req.body.password) {
-            const payload = {
-                check:  true
-            };
-            const token = jwt.sign(payload, config.masterKeyGesSalud, {
-                expiresIn: 1440
-            });
-            res.json({
-                    token: token,
-                    persona: register.dataValues,
-            });
-        } else {
-            res.json({ mensaje: "Usuario o contraseña incorrectos"})
-        }
+  if (persona) {
+    register = persona.dataValues;
+  }
+
+  let datosEspec = { dataValues: "" };
+  if (persona && persona.dataValues.rol === "3") {
+    datosEspec = await Especialista_medico.findOne({
+      where: { personaId: persona.dataValues.id },
+      attributes: ["id"],
+    });
+    especialistaId = datosEspec.dataValues.id;
+    register = { ...register, especialistaId: especialistaId };
+  }
+
+  console.log("################", register);
+  //verificar rol 3
+  //adjuntar el especialista_id
+
+  if (register) {
+    if (register.password === req.body.password) {
+      const payload = {
+        check: true,
+      };
+      const token = jwt.sign(payload, config.masterKeyGesSalud, {
+        expiresIn: 1440,
+      });
+      res.json({
+        token: token,
+        persona: register,
+      });
+    } else {
+      res.json({ mensaje: "Usuario o contraseña incorrectos" });
     }
-    else {
-        res.json({ mensaje: "Usuario o contraseña incorrectos"})
-    }
+  } else {
+    res.json({ mensaje: "Usuario o contraseña incorrectos" });
+  }
 });
 
 module.exports = router;
