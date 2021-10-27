fe56f81f10786bf5413dfa28413d1b3291eda2aa
marfa65 <marfa.gonzalez@hotmail.com>
2021-10-27 20:31:16 -0300

    implementacion de imagenes

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -415,3 +415,21 @@ export const modificarDiagnostico = (diagnostico) => {
     }
   };
 };
+
+//SUBIR IMAGEN
+export const uploadAction = async (image) => {
+  const fd = new FormData();
+  fd.append("image", image);
+
+  const config = {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  };
+
+  try {
+    const res = await axios.post("http://localhost:3001/images", fd, config);
+  } catch (err) {
+    console.log(err);
+  }
+};
