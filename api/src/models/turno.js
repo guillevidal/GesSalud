2242431dc3307b7fe4dee1a015099160a01c1b88
unknown <fernandodemaio@outlook.es>
2021-10-31 17:13:12 -0300

    agregamos campo de texto en tabla hc

--- api/src/models/turno.js
+++ api/src/models/turno.js
@@ -1,23 +1,30 @@
-const { DataTypes, STRING } = require('sequelize');
+const { DataTypes, STRING } = require("sequelize");
 
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('turno', {
-    status: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  sequelize.define(
+    "turno",
+    {
+      status: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      hour: {
+        type: DataTypes.STRING,
+        //allowNull: false,
+      },
+      modules: {
+        type: DataTypes.INTEGER, ////acepta valores multiplos de 15
+        //allowNull: false,
+      },
+      payment: {
+        type: DataTypes.BOOLEAN, ////acepta valores multiplos de 15
+        defaultValue: false,
+        //allowNull: false,
+      },
     },
-    hour: {
-      type: DataTypes.STRING,
-      //allowNull: false,
-    },
-    modules: {
-      type: DataTypes.INTEGER,  ////acepta valores multiplos de 15
-      //allowNull: false,
-    }
-    }, 
-  { timestamps: false }
-  ); 
+    { timestamps: false }
+  );
 };
