f51857c2cdc929f41a3d27897c9a187456c282d9
roezna <rodrigoezequielnavarro99@gmail.com>
2021-11-04 18:15:28 -0300

    agrego boton en pagina de error

--- client/src/components/Landing/NotFound.jsx
+++ client/src/components/Landing/NotFound.jsx
@@ -2,7 +2,7 @@ import './Landing.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faClinicMedical, faExclamationTriangle, faDizzy } from '@fortawesome/free-solid-svg-icons';
 import { useHistory } from 'react-router';
-
+import { Link } from 'react-router-dom';
 export default function NotFound(){
 
   const back = useHistory()
@@ -22,7 +22,8 @@ export default function NotFound(){
       <h2 className='title'>Página no encontrada <FontAwesomeIcon icon={faDizzy} className='dizzy'/></h2>
       <span className='detail'>Lamentablemente no encontramos acceso a esta ruta, puedes volver atrás y continuar navegando en nuestra aplicación.</span>
       
-        <div><button className='vuelta' onClick={() => back.goBack()}>Volver</button></div>
+        <div className='BOTONES'><button className='vuelta' onClick={() => back.goBack()}>Volver</button>
+        <Link to='/'><button className='vuelta' >Página principal</button></Link></div>
       </div>
 
         </div>
